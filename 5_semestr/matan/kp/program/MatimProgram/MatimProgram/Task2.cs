be9d439530acff7b236b5ab473a514774e48ba95
Gena <malkerovg99@gmail.com>
2022-01-23 00:17:33 +0300

    работающее задание 2 не на numpy

--- 5_semestr/matan/kp/program/MatimProgram/MatimProgram/Task2.cs
+++ 5_semestr/matan/kp/program/MatimProgram/MatimProgram/Task2.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 
 using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Double;
-
-using NumSharp;
 
 namespace MatimProgram
 {
@@ -15,40 +12,40 @@ namespace MatimProgram
         private readonly double _m = 3.0;
         private readonly double _u = 6.0;
         private readonly double _n = 13.0;
-
-        private readonly int _lInt = 14;
+        
         private readonly int _mInt = 3;
         private readonly int _uInt = 6;
         private readonly int _nInt = 13;
-        
+
         public void Make()
         {
-            var matrixIntensity = np.zeros(_mInt + _nInt + 1, _mInt + _nInt + 1);
+            var matrixNew = Matrix<double>.Build.Dense(_mInt + _nInt + 1, _mInt + _nInt + 1);
+
 
             for (var i = 0; i < _mInt + _nInt; i++)
             {
-                matrixIntensity[i, i + 1] = _l;
+                matrixNew[i, i + 1] = _l;
                 if (i < _mInt)
-                    matrixIntensity[i + 1, i] = (_uInt * (i + 1));
+                    matrixNew[i + 1, i] = (_uInt * (i + 1));
                 else
-                    matrixIntensity[i + 1, i] = _u * _m;
+                    matrixNew[i + 1, i] = _u * _m;
             }
             Console.WriteLine("Задание А.");
             Console.WriteLine("Матрица интенсивности переходов");
-            matrixIntensity.PrintNdArrayToConsole();
+            matrixNew.PrintToConsole();
 
             Console.WriteLine();
             Console.WriteLine("Задание B.");
             Console.WriteLine("Посчтитаем  диаганальную матрицу D");
 
-            var matrixSumm = new int[_mInt + _nInt + 1];
+            var matrixSumm = new double[_mInt + _nInt + 1];
 
-            for (var i = 0; i < matrixIntensity.Shape[0]; i++)
+            for (var i = 0; i < matrixNew.RowCount; i++)
             {
-                var sum = 0;
-                for (var j = 0; j < matrixIntensity[i].Shape[0]; j++)
+                var sum = 0.0;
+                for (var j = 0; j < matrixNew.ColumnCount; j++)
                 {
-                    sum += int.Parse(matrixIntensity[i, j].ToString());
+                    sum += matrixNew[i, j];
                 }
                 matrixSumm[i] = sum;
             }
@@ -56,10 +53,11 @@ namespace MatimProgram
             Console.WriteLine("Сумма строк матрицы интенсивности");
             matrixSumm.PrintArray();
             Console.WriteLine("Размещаем ее на диагонали");
-            var matrixD = np.zeros(_mInt + _nInt + 1, _mInt + _nInt + 1);
-            for (var i = 0; i < matrixD.Shape[0]; i++)
+            var matrixD = Matrix<double>.Build.Dense(_mInt + _nInt + 1, _mInt + _nInt + 1);
+
+            for (var i = 0; i < matrixD.RowCount; i++)
             {
-                for (var j = 0; j < matrixD[i].Shape[0]; j++)
+                for (var j = 0; j < matrixD.ColumnCount; j++)
                 {
                     if (i == j)
                     {
@@ -68,17 +66,17 @@ namespace MatimProgram
                 }
             }
 
-            matrixD.PrintNdArrayToConsole();
-
-            var matrixM = matrixIntensity.T - matrixD;
+            matrixD.PrintToConsole();
+            matrixNew.Transpose().PrintToConsole();
+            var matrixM = matrixNew.Transpose() - matrixD;
 
             Console.WriteLine("Матрица М");
-            matrixM.PrintNdArrayToConsole();
+            matrixM.PrintToConsole();
             // ReSharper disable once InconsistentNaming
             var matrixM_ = matrixM;
-            for (var i = 0; i < matrixM_.Shape[0]; i++)
+            for (var i = 0; i < matrixM_.RowCount; i++)
             {
-                for (var j = 0; j < matrixM_[i].Shape[0]; j++)
+                for (var j = 0; j < matrixM_.ColumnCount; j++)
                 {
                     if (i == _mInt + _nInt)
                     {
@@ -87,28 +85,27 @@ namespace MatimProgram
                 }
             }
             Console.WriteLine("Матрица М_");
-            matrixM_.PrintNdArrayToConsole();
+            matrixM_.PrintToConsole();
 
             Console.WriteLine("Матрица B");
-            var bMatrix = np.zeros(matrixM_.Shape[0]);
-            bMatrix[matrixM_.Shape[0] - 1] = 1;
-            bMatrix.PrintNdArrayToConsole();
+            var bMatrix = Matrix<double>.Build.Dense(_mInt + _nInt + 1, 1);
+            bMatrix[bMatrix.RowCount - 1, 0] = 1;
+            bMatrix.PrintToConsole();
 
 
             Console.WriteLine("Матрица X");
-            var matr = MatrixToTemp(matrixM_);
-            
-            var inv = matr.Inverse();
-            ConvertToNumpyMatrix(inv).PrintNdArrayToConsole();
-            var x = ConvertToNumpyMatrix(inv) * bMatrix;
+
+            var inv = matrixM_.Inverse();
+            inv.PrintToConsole();
+            var x = inv * bMatrix;
 
             var xNormal = new double[_mInt + _nInt + 1];
 
-            for (var i = 0; i < x.Shape[0]; i++)
+            for (var i = 0; i < x.RowCount; i++)
             {
-                for (var j = 0; j < x[i].Shape[0]; j++)
+                for (var j = 0; j < x.ColumnCount; j++)
                 {
-                    if (j == x[i].Shape[0] - 1)
+                    if (j == x.ColumnCount - 1)
                         xNormal[i] = x[i, j];
                 }
             }
@@ -152,19 +149,19 @@ namespace MatimProgram
 
             for (var i = _mInt + 1; i < _mInt + _nInt + 1; i++)
             {
-                resOfRight.Add(_m*xNormal[i]);
+                resOfRight.Add(_m * xNormal[i]);
             }
 
             Console.WriteLine($"Сумма левой части [{resOfLeft.Sum()}]");
             Console.WriteLine($"Сумма правой части [{resOfRight.Sum()}]");
-            Console.WriteLine($"Среднее число занятых каналов [{resOfLeft.Sum()+ resOfRight.Sum()}]");
+            Console.WriteLine($"Среднее число занятых каналов [{resOfLeft.Sum() + resOfRight.Sum()}]");
 
 
             Console.WriteLine();
             Console.WriteLine("Задание G.");
 
             var resOf = new List<double>();
-            for (var i = 0; i < _m-1; i++)
+            for (var i = 0; i < _m - 1; i++)
             {
                 resOf.Add(xNormal[i]);
             }
@@ -172,36 +169,7 @@ namespace MatimProgram
 
             Console.WriteLine();
             Console.WriteLine("Задание H.");
-            Console.WriteLine($"Среднее время простоя системы массового обслуживания [{1.0/_l}]");
-        }
-
-        private DenseMatrix MatrixToTemp(NDArray array)
-        {
-            var res = new DenseMatrix(array.Shape[0], array.Shape[0]);
-            for (var i = 0; i < array.Shape[0]; i++)
-            {
-                for (var j = 0; j < array[i].Shape[0]; j++)
-                {
-                    res[i, j] = array[i, j];
-                }
-            }
-
-            return res;
-        }
-
-        private NDArray ConvertToNumpyMatrix(Matrix<double> matrix)
-        {
-            var res = np.zeros(matrix.RowCount, matrix.ColumnCount);
-
-            for (var i = 0; i < matrix.RowCount; i++)
-            {
-                for (var j = 0; j < matrix.ColumnCount; j++)
-                {
-                    res[i, j] = matrix[i, j];
-                }
-            }
-
-            return res;
+            Console.WriteLine($"Среднее время простоя системы массового обслуживания [{1.0 / _l}]");
         }
     }
 }
