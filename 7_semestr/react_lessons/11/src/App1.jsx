efb5321915cb43d789d2f1b14eda6dd141c6346b
malkerovga <>
2022-12-01 14:56:46 +0300

    работы по вебу

---
+++ 7_semestr/react_lessons/11/src/App1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App1() {
+  return (
+      <div className="App1">
+        <h3>Путешествие</h3>
+        <h2>Лучшие авиалинии для комфортного путешествия эконом-классом</h2>
+      </div>
+  );
+}
